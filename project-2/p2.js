3933a6728c7148463e8785a8e1e206d7dc38d048
blue-harry <96937529+blue-harry@users.noreply.github.com>
2025-01-18 19:28:03 +0800

    + random

--- project-2/p2.js
+++ project-2/p2.js
@@ -9,21 +9,19 @@ let frog = new Image()
 frog.src = "./frog.png"
 
 let frogY = board.height - 100
-let frogVel = -30
+let frogVel = -20
+let gravity = 0.6
 
-// requestAnimationFrame update{}
 const update = () => {
   requestAnimationFrame(update)
 
-  if (frogY <= board.height - 100) {
-    context.clearRect(0, 0, board.width, board.height)
-    frogVel += 1
-    frogY = frogY + frogVel
+  if (frogY > board.height - 100) {
+    frogVel = Math.random() * -32
   }
-
+  context.clearRect(0, 0, board.width, board.height)
+  frogVel += gravity
+  frogY += frogVel
   context.drawImage(frog, 10, frogY, 100, 100)
-
-  // frogY = frogY + 1
 }
 
 update()
