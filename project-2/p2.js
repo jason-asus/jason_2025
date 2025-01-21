4e764faea8b2cae804cce41d4d2c687f01c87ae4
blue-harry <96937529+blue-harry@users.noreply.github.com>
2025-01-21 18:56:53 +0800

    - {}

--- project-2/p2.js
+++ project-2/p2.js
@@ -5,23 +5,58 @@ board.height = window.innerHeight
 
 const context = board.getContext("2d")
 
-let frog = new Image()
-frog.src = "./frog.png"
-
-let frogY = board.height - 100
-let frogVel = -20
 let gravity = 0.6
 
+/** frog */
+const frog = {
+  img: new Image(),
+  x: 0 + 100,
+  y: board.height - 100,
+  width: 100,
+  height: 100,
+  velocity: -20,
+}
+
+frog.img.src = "./frog.png"
+
+/** grasshopper */
+const grasshopper = {
+  img: new Image(),
+  x: 100 + 100,
+  y: board.height - 100,
+  width: 100,
+  height: 100,
+  velocity: -20,
+}
+grasshopper.img.src = "./grasshopper.png"
+
+// let grasshopper = new Image()
+
+/** draw */
 const update = () => {
   requestAnimationFrame(update)
 
-  if (frogY > board.height - 100) {
-    frogVel = Math.random() * -32
+  if (frog.y > board.height - 100) {
+    frog.velocity = Math.random() * -32
+  }
+  if (grasshopper.y > board.height - 100) {
+    grasshopper.velocity = Math.random() * -31.99
   }
   context.clearRect(0, 0, board.width, board.height)
-  frogVel += gravity
-  frogY += frogVel
-  context.drawImage(frog, 10, frogY, 100, 100)
+  frog.velocity += gravity
+  frog.y += frog.velocity
+
+  grasshopper.velocity += gravity
+  grasshopper.y += grasshopper.velocity
+
+  context.drawImage(frog.img, frog.x, frog.y, frog.width, frog.height)
+  context.drawImage(
+    grasshopper.img,
+    grasshopper.x,
+    grasshopper.y,
+    grasshopper.width,
+    grasshopper.height
+  )
 }
 
 update()
