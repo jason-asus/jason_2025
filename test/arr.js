e311014b28f138b9b1366aa922b969e667e3b39a
blue-harry <96937529+blue-harry@users.noreply.github.com>
2025-02-13 14:54:05 +0800

    test：增加内容

--- test/arr.js
+++ test/arr.js
@@ -7,3 +7,28 @@ const f2 = (n) => n.toString().padStart(2, "0")
 const f3 = () => `[ ${a1.map(f1).join(".")} ] @ ${a2.map(f2).join(":")}`
 
 console.log(f3())
+
+/** 实际代码：标识有新留言的预约单  */
+
+//我的原代码
+if (
+  appt?.message?.lines.find((line) => {
+    return (
+      !appt.message.customerLastRead ||
+      (line[0] == "staff" && line[2] > appt.message.customerLastRead)
+    )
+  })
+) {
+  appt.hasNewMsg = true
+}
+
+//她修改过的代码
+const lines = appt?.message?.lines.find((line) => {
+  return (
+    !appt.message.customerLastRead ||
+    (line[0] == "staff" && line[2] > appt.message.customerLastRead)
+  )
+})
+if (!!lines) {
+  appt.hasNewMsg = true
+}
--- test/questions.md
+++ test/questions.md
@@ -1,2 +1,11 @@
+# 微信小程序
+
 - 小程序如何存储全局数据？有几种方式？
 - 小程序如何将一个对象传给下一个页面？
+
+# CSS
+
+- position
+- flex-grow:1
+- inline inline-block
+- rem em rpx px
